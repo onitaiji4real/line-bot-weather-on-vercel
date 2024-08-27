a0d4a8490811ab48f6c6d080f027ec3ee64715dd
Hank_Chen <hank_chen@rosati.com.tw>
2024-08-27 13:23:17 +0800

    feat: add index.js

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
+require("dotenv").config();
 const express = require("express");
-const { client, middleware } = require("@line/bot-sdk");
+const { Client, middleware } = require("@line/bot-sdk");
+const helloTestRouter = require("./routes/hello");
 const app = express();
 
-//Line Bot configuration
+// Line Bot configuration
 const config = {
-  channalAccessToken: "CHANNEL_ACCESS_TOKEN",
-  channalSecret: "CHANNEL_SECRET",
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
-//Create a new line client
+// Create a new line client
 const client = new Client(config);
 
-//Middleware for validating request from line platform
-app.use(middleware(config));
-
-app.post("/webhook", (req, res) => {
-  Promise.all(req.body.event.map(handleEvent)).then((result) =>
-    res.json(result).catch((err) => {
+// Middleware for validating requests from Line platform
+app.post("/webhook", middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
       console.error(err);
       res.status(500).end();
-    })
-  );
+    });
 });
 
+// Test route for make sure server is live
+app.use("/hello", helloTestRouter);
+
 function handleEvent(event) {
-  if (event.type !== "message" || event.message.type !== "text")
+  if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
+  }
 
-  //create a response message
-  const echo = {
-    type: "text",
-    text: event.message.text,
-  };
+  // Create a response message
+  const echo = { type: "text", text: event.message.text };
 
-  //use line client to reply to the user
+  // Use Line client to reply to the user
   return client.replyMessage(event.replyToken, echo);
 }
 
